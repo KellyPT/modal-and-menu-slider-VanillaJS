4ab6d2e4f177e8023adb296ab43b12ee6a813787
KellyPT <trankphuong@gmail.com>
2020-03-25 16:22:07 -0700

    use prettier to format with singleQuote

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-const toggle = document.getElementById("toggle");
-const close = document.getElementById("close");
-const open = document.getElementById("open");
-const modal = document.getElementById("modal");
+const toggle = document.getElementById('toggle');
+const close = document.getElementById('close');
+const open = document.getElementById('open');
+const modal = document.getElementById('modal');
 
 // toggle nav bar
-toggle.addEventListener("click", () =>
-  document.body.classList.toggle("show-nav")
+toggle.addEventListener('click', () =>
+  document.body.classList.toggle('show-nav'),
 );
 
 // show modal
-open.addEventListener("click", () => modal.classList.add("show-modal"));
+open.addEventListener('click', () => modal.classList.add('show-modal'));
 
 // hide modal
-close.addEventListener("click", () => modal.classList.remove("show-modal"));
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
 
 // hide modal on clicks on other positions
-window.addEventListener("click", e =>
-  e.target == modal ? modal.classList.remove("show-modal") : false
+window.addEventListener('click', e =>
+  e.target == modal ? modal.classList.remove('show-modal') : false,
 );
